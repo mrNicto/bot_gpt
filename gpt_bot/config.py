59ca1566563a0c41ce00930bb8329a16895f862d
unknown <you@example.com>
2024-05-14 21:27:30 +0300

    Ого!

---
+++ gpt_bot/config.py
@@ -0,0 +1,26 @@
+HOME_DIR = '/home/student/gpt_bot'  # путь к папке с проектом
+LOGS_PATH = f'{HOME_DIR}/bot_logs.txt'  # файл для логов
+DB_FILE = f'{HOME_DIR}/speech.db'  # файл для базы данных
+
+IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam_token.txt'  # файл для хранения iam_token
+FOLDER_ID_PATH = f'{HOME_DIR}/creds/folder_id.txt'  # файл для хранения folder_id
+BOT_TOKEN_PATH = f'{HOME_DIR}/creds/bot_token.txt'  # файл для хранения bot_token 
+
+TOKEN = '6966885742:AAFocCOS1nOzz-AT1yRMkan7m6AD-DlyBH4'  # токен для ТГ бота
+
+IAM_TOKEN = 't1.9euelZqJjMeTj8uNisqNjYuLl8vNne3rnpWajMuXlZaYiZjHmI3JmcnPncbl9PdfF3JN-e8TcXyr3fT3H0ZvTfnvE3F8q83n9euelZrLyZrIk5iZjJCLjcmUx8-Mi-_8xeuelZrLyZrIk5iZjJCLjcmUx8-Mi73rnpWajZHHk8_OjZ7NxpaYnMmJyMa13oac0ZyQko-Ki5rRi5nSnJCSj4qLmtKSmouem56LntKMng.3sAOgH5G__lsYya2UUZJB2wlIoTJcAu9Ux_faANNXGWsK3WmVpS7vBIO-Vxc_aBJ4cwL6JQHPI_dPljZjjbaAw' #вставти ваш IAM_TOKEN
+FOLDER_ID = 'b1gq6o5l93te5s4h44i0' #укажите ваш FOLDER_ID
+
+VOICE = "filipp"
+MAX_LEN = 30
+
+GPT_MODEL = 'yandexgpt-lite'
+MODEL_TEMPERATURE = 0.6
+MAX_MODEL_TOKENS = 100
+LOGS_PATH = 'bot_logs.txt'
+
+ADMIN_ID = '5050005208'
+
+MAX_USER_STT_BLOCKS = 30
+
+MAX_USER_TOKENS_BLOCKS = 8000
